08bece5a56c8984e52734d9552ce1b32ee62ba98
ryanycw <ryanycw@users.noreply.github.com>
2024-10-24 23:15:52 +0800

    feat: initial commit

---
+++ src/NonFungiblePosVault.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import { SolidStateDiamond } from "solidstate-solidity/proxy/diamond/SolidStateDiamond.sol";
+
+contract NonFungiblePosVault is SolidStateDiamond { }
